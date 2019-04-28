a9e78cbb6fe7615ce4977976a5c3d3c3905de8eb
imgomez0127 <ianm0127@hotmail.com>
2019-04-28 19:59:39 -0400

    Added another user function and fixed the username typo

--- mongodbAPI/userOperations.js
+++ mongodbAPI/userOperations.js
@@ -3,11 +3,20 @@ const connection = require("./establishConnection");
 const ObjectID = require("mongodb").ObjectID
 
 let getAllUsers = async function(){
-    const users = await users();
+    const users = await usersCollection();
     const allUsers = await users.find({}).toArray();
     return allUsers;
 }
 
+let getUserByUsername = async function(username){
+    if(typeof(username) != "string"){
+        throw new Error("The input username is not a string");
+    }
+    const users = await usersCollection();
+    const user = await users.find({"username":username}).
+    return user
+}
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUserByUsername
 }
