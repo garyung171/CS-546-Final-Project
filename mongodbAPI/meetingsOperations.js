5e44709e3a4cf4748939e774e69e5617d1229ad7
imgomez0127 <ianm0127@hotmail.com>
2019-05-17 16:59:56 -0400

    Added function to add meetings

--- mongodbAPI/meetingsOperations.js
+++ mongodbAPI/meetingsOperations.js
@@ -3,3 +3,30 @@ const connection = require("./establishConnection");
 const ObjectID = require("mongodb").ObjectID;
 const slugify = require("slugify");
 
+let createMeeting = async function(meetupName,owner,date,location){
+    if(!meetingsCollection || !owner || !date || !location){
+        return false;
+    }
+    else{
+        const meetings = await meetingsCollection();
+        const users = await usersCollection();
+        const ownerID = await getUserByUsername(username)["_id"];
+        if(!ownerID){
+            return false;
+        }
+        let meeting = {
+                        meetupName:meetupName,
+                        owner:ownerID,
+                        attendees:[ownerID], 
+                        date:date,
+                        location:location,
+                        comments:[]
+                    } 
+        const insertInfo = await meetings.insertOne(meeting);
+        if(insertInfo.insertedCount === 0){
+            throw "Unable to create user."
+        }
+        return true;
+    }
+} 
+        
