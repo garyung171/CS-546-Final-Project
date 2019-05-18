3b597e9dd8460b2267eff4bc2a691b4bc5d8fed6
SHAPS252 <dshap252@gmail.com>
2019-05-18 15:15:42 -0400

    Fixed seed

--- seed.js
+++ seed.js
@@ -14,11 +14,13 @@ async function main(){
         let password1 = await bcrypt.hash("blah1",saltRounds);
         let password2 = await bcrypt.hash("blah2",saltRounds);
         await userOperations.createUser("masterdetective123", password1, "Hoboken", "blah@blah.com");
+        await userOperations.updatePreferences("masterdetective123", ["Smite", "DotA 2", "Risk of Rain 2"]);
         let detective = await userOperations.getUserByUsername("masterdetective123");
         await userOperations.createUser("lemon", password2, "Hoboken", "blah@blah.com");
+        await userOperations.updatePreferences("lemon", ["Smite", "Dungeons and Dragons"]);
         let lemon = await userOperations.getUserByUsername("lemon");
         let meeting1 = await meetingsOperations.createMeeting("Smite1", detective._id, new Date(), "Hoboken", ["Smite"]);
-        await meetingsOperations.createMeeting("Dota1", detective._id, new Date(), "Hoboken", ["DOTA2"]);
+        await meetingsOperations.createMeeting("Dota1", detective._id, new Date(), "Hoboken", ["DotA2"]);
         await meetingsOperations.createMeeting("Mixed", detective._id, new Date(), "Hoboken", ["Smite", "Dota"]);
         await meetingsOperations.createMeeting("Far", detective._id, new Date(), "New York", ["Smite"]);
         await meetingsOperations.createMeeting("Lonely", lemon._id, new Date(), "Hoboken", ["Smite"]);
